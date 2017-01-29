9e8cac89f86d6f365b65ebcdc8d3adc10eb0c1d6
Xiao Ye Chen <xchen389@uwo.ca>
2017-01-29 02:07:27 -0500

    Map change

---
+++ deltahacks3-vr/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public int initialScore = 0;
+    public Text scoreText;
+
+    int currentScore;
+
+	// Use this for initialization
+	void Start () {
+        currentScore = initialScore;
+        scoreText.text = "SCORE: 0";
+	}
+	
+    void Update()
+    {
+        scoreText.text = "SCORE: " + currentScore;
+    }
+
+	public void AddScore(int amount)
+    {
+        currentScore += amount;
+    }
+}
