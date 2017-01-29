592f2b9066b07070d6b0af583f562cf7ef1ce616
Michael Le <zerglinglover@gmail.com>
2017-01-29 10:53:12 -0500

    exit final  final push

---
+++ deltahacks3-vr/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Hand")
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        }
+
+    }
+}
