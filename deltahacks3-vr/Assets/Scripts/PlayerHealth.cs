989fda3aece9aa7bcf1d76502eb85b86d445933a
Michael Le <zerglinglover@gmail.com>
2017-01-29 09:56:09 -0500

    Revert "final push"
    This reverts commit 93b82517be8f4f16661a4f6470b0735e9ddc9ee3.

--- deltahacks3-vr/Assets/Scripts/PlayerHealth.cs
+++ deltahacks3-vr/Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
     public Image damageImage;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
-    public int health;
-    public Text healthText;
     bool damaged;
     // Use this for initialization
     void Start () {
-        healthText.text = "Health: 0";
+        
 	}
 	
 	// Update is called once per frame
@@ -21,18 +18,12 @@ public class PlayerHealth : MonoBehaviour {
 	    if (damaged)
         {
             damageImage.color = flashColour;
-            health--;
         }
         else
         {
             damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
         damaged = false;
-        healthText.text = "Health: " + health;
-
-        if (health <= 0)
-            SceneManager.LoadScene(0);
-
     }
 
     public void GotHit()
